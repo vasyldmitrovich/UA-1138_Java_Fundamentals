7bcc23572d38cf8f4cd2b08c65726657970f8496
obutr <butrym@karazin.ua>
2024-01-22 20:45:23 +0200

    Add Task2 and add task2() method into hw1.App.java

--- src/com/softserve/edu01/hw/App.java
+++ src/com/softserve/edu01/hw/App.java
@@ -12,7 +12,8 @@ public class App {
         // Task 1
         App.task1();
 
-        // Task 2...
+        // Task 2
+        task2();
     }
 
     public static void task1 () {
@@ -20,5 +21,17 @@ public class App {
         String name = scanner.nextLine();
 
         System.out.println("Your name is: " + name);
+    }
+    public static void task2 () {
+        System.out.println("Input first string");
+        String first = scanner.nextLine();
+        System.out.println("Input second string");
+        String second = scanner.nextLine();
+        System.out.println("Input third string");
+        String third = scanner.nextLine();
+
+        System.out.println(third);
+        System.out.println(second);
+        System.out.println(first);
     }
 }
