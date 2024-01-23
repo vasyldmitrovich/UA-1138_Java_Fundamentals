17c0ac3914082a78951ffb2ca5328b0093a577f4
ANIMAL <ilya.bugay01@gmail.com>
2024-01-23 23:08:29 +0200

    Add methods task1() and task2() into hw1.App.java

--- src/com/softserve/edu01/hw/App.java
+++ src/com/softserve/edu01/hw/App.java
@@ -8,17 +8,26 @@ public class App {
 
     public static void main(String[] args) {
         // Homeworks
-
         // Task 1
-        App.task1();
-
-        // Task 2...
+        //App.task1();
+        // Task 2
+        App.task2();
     }
 
     public static void task1 () {
-        System.out.println("Input your name");
-        String name = scanner.nextLine();
+        System.out.println("Input radius of the circle: ");
+        double radius = scanner.nextDouble();
+        final double pi = Math.PI;
+        double s = pi * Math.pow(radius, 2);
+        double p = 2 * pi * radius;
+        System.out.println("Square of the circle: " + s + "\nPerimeter of the circle: " + p);
+    }
 
-        System.out.println("Your name is: " + name);
+    public static void task2 () {
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+        System.out.println("Where dou you live, " + name + "?");
+        String address = scanner.nextLine();
+        System.out.println(name + " lives at " + address);
     }
 }
