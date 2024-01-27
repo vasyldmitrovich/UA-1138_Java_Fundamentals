d768017710be25a55ab817cf5c2299d55218fa68
gannasamokhval <ganna.samokhval@gmail.com>
2024-01-27 23:45:39 +0200

    HW3

---
+++ src/com/softserve/edu03/hw/AreaOfTriangle.java
@@ -0,0 +1,27 @@
+package com.softserve.edu03.hw;
+
+import java.util.Scanner;
+
+public class AreaOfTriangle {
+    static public final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Input Side 1:");
+        double side1 = SCANNER.nextDouble();
+
+        System.out.println("Input Side 2:");
+        double side2 = SCANNER.nextDouble();
+
+        System.out.println("Input Side 3:");
+        double side3 = SCANNER.nextDouble();
+
+        double area = calculateArea(side1, side2, side3);
+        System.out.println("The area of the triangle is: " + area);
+    }
+
+    private static double calculateArea(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+}
---
+++ src/com/softserve/edu03/hw/SmallestNumber.java
@@ -0,0 +1,33 @@
+package com.softserve.edu03.hw;
+
+import java.util.Scanner;
+
+public class SmallestNumber {
+    static public final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Input the first number:");
+        int first = SCANNER.nextInt();
+
+        System.out.println("Input the second number:");
+        int second = SCANNER.nextInt();
+
+        System.out.println("Input the third number:");
+        int third = SCANNER.nextInt();
+
+        int min = findMin(first, second, third);
+        System.out.println("The smallest number is: " + min);
+    }
+
+    private static int findMin(int first, int second, int third) {
+        int min = first;
+        if (second < min) {
+            min = second;
+        }
+        if (third < min) {
+            min = third;
+        }
+
+        return min;
+    }
+}
