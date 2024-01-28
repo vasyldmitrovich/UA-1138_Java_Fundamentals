eb22e98e8653265af701988a03e8ebb01afdae75
Denis_Golovchenko <denisgolovchenko02@gmail.com>
2024-01-28 18:59:59 +0200

    Add tasks from first topic

--- src/com/softserve/edu01/hw/App.java
+++ src/com/softserve/edu01/hw/App.java
@@ -14,15 +14,58 @@ public class App {
 
         // Task 2...
         App.task2();
+
+        // Task 3
+        task3();
     }
 
     public static void task1 () {
-        System.out.println("Input your name");
-        String name = scanner.nextLine();
+        double pi = 3.14159;
 
-        System.out.println("Your name is: " + name);
+        System.out.println("Input radius:");
+        float radius = scanner.nextFloat();
+
+        double  perimeter = 2 * pi * radius;
+        double area =  pi * radius * radius;
+
+        System.out.println("Perimeter = " + perimeter);
+        System.out.println("Area = " + area);
     }
     public static void task2 () {
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
 
+        System.out.println("Where do you live " + name + "?");
+        String address = scanner.nextLine();
+
+        System.out.println("Your name is " + name);
+        System.out.println("Your address is " + address);
+    }
+    public static void task3() {
+
+        System.out.println("Input the cost of the first call: ");
+        double c1 = scanner.nextDouble();
+        System.out.println("Input the cost of the second call: ");
+        double c2 = scanner.nextDouble();
+        System.out.println("Input the cost of the third call: ");
+        double c3 = scanner.nextDouble();
+
+        System.out.println("Input the time of the first call: ");
+        double t1 = scanner.nextDouble();
+        System.out.println("Input the time of the second call: ");
+        double t2 = scanner.nextDouble();
+        System.out.println("Input the time of the third call: ");
+        double t3 = scanner.nextDouble();
+
+        System.out.println("Cost of each call ");
+        double costOfCall1 = c1 * t1;
+        System.out.println("call №1: " + costOfCall1);
+        double costOfCall2 = c2 * t2;
+        System.out.println("call №1: " + costOfCall2);
+        double costOfCall3 = c3 * t3;
+        System.out.println("call №1: " + costOfCall3);
+
+        double totalCoast = costOfCall1 + costOfCall2 + costOfCall3;
+        System.out.println("Total coast: " + totalCoast);
     }
 }
--- src/com/softserve/edu01/pt/App.java
+++ src/com/softserve/edu01/pt/App.java
@@ -1,15 +1,74 @@
 package com.softserve.edu01.pt;
 
+import java.util.Scanner;
+
 public class App {
+
+    public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+    }
+    public static void  task1(){
+        String firstString;
+        String secondString;
+        String thirdString;
 
-        // Practical tasks
+        System.out.println("Input text:");
+        firstString = scanner.nextLine();
+        secondString = scanner.nextLine();
+        thirdString = scanner.nextLine();
 
-        // Task 1
+        System.out.println(thirdString);
+        System.out.println(secondString);
+        System.out.println(firstString);
+    }
 
-        // Task 2
+    public static void task2(){
+        float firstNumber;
+        float secondNumber;
+        float thirdNumber;
+        double mid;
 
-        // Task 3...
+        System.out.println("Input first number:");
+        firstNumber = scanner.nextFloat();
+        System.out.println("Input second number:");
+        secondNumber = scanner.nextFloat();
+        System.out.println("Input third number:");
+        thirdNumber = scanner.nextFloat();
 
+        mid = (firstNumber + secondNumber+ thirdNumber)/3;
+        System.out.println("Average is "+ mid);
+    }
+    public static void task3(){
+        float a;
+        float b;
+        double c;
+
+        System.out.println("Input values a:");
+        a = scanner.nextFloat();
+        System.out.println("Input values b:");
+        b = scanner.nextFloat();
+
+        c = a+b;
+        System.out.println("a + b = " + c);
+
+        c= a-b;
+        System.out.println("a - b = " + c);
+
+        c= a*b;
+        System.out.println("a * b = " + c);
+
+        c= a/b;
+        System.out.println("a / b = " + c);
+    }
+    public static void task4(){
+        String answer;
+
+        System.out.println("How are you?");
+        answer = scanner.nextLine();
+        System.out.println("You are " + answer);
     }
 }
