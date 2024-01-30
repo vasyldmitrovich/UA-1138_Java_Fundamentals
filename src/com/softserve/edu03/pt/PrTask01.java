27cad90729d69469d4745c9677f120fedc53be22
gannasamokhval <ganna.samokhval@gmail.com>
2024-01-31 00:12:00 +0200

    HW3

---
+++ src/com/softserve/edu03/pt/PrTask01.java
@@ -0,0 +1,31 @@
+package com.softserve.edu03.pt;
+
+import java.util.Scanner;
+
+public class PrTask01 {
+    public static void main(String[] args) {
+        int first = getNumber("Enter the first number: ");
+        int second = getNumber("Enter the second number: ");
+        int sum = getTotal(first, second);
+        double average = getAverage(first, second);
+        System.out.println("The sum of " + first +
+                " and " + second + " is " + sum);
+        System.out.println("The average of " + first +
+                " and " + second + " is " + average);
+    }
+
+    private static int getNumber(String prompt) {
+        System.out.print(prompt);
+        Scanner scan = new Scanner(System.in);
+        return Integer.parseInt(scan.nextLine());
+    }
+
+    private static double getAverage(int first, int second) {
+        return getTotal(first, second) / 2.0;
+    }
+
+    private static int getTotal(int first, int second) {
+        return first + second;
+    }
+
+}
