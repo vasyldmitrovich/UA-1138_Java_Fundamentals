3ec66b4afc87ca41b8dd3d55901b1b0eb4519618
GlushakOlga <oliag22@gmail.com>
2024-01-31 01:13:34 +0200

    Add Pt edu01 Add HW edu01

--- src/com/softserve/edu01/hw/App.java
+++ src/com/softserve/edu01/hw/App.java
@@ -7,30 +7,18 @@ public class App {
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        // Homeworks
+        System.out.print("Enter the radius of the flower bed: ");
+        double radius = scanner.nextDouble();
 
-        // Task 1
-        App.task1();
-
-        // Task 2...
+        double perimeter = calculatePerimeter(radius);
+        double area = calculateArea(radius);
     }
 
-    public static void task1 () {
-        System.out.println("Input your name");
-        String name = scanner.nextLine();
-
-        System.out.println("Your name is: " + name);
+    private static double calculateArea() {
+        return 0;
     }
-    public static void task2 () {
-        System.out.println("Input first string");
-        String first = scanner.nextLine();
-        System.out.println("Input second string");
-        String second = scanner.nextLine();
-        System.out.println("Input third string");
-        String third = scanner.nextLine();
 
-        System.out.println(third);
-        System.out.println(second);
-        System.out.println(first);
+    private static double calculateArea() {
+        return 0;
     }
 }
--- src/com/softserve/edu01/pt/App.java
+++ src/com/softserve/edu01/pt/App.java
@@ -1,15 +1,47 @@
 package com.softserve.edu01.pt;
 
+import java.util.Scanner;
+
 public class App {
+    public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
         // Practical tasks
+        //Task 1
+        /*
+        System.out.println("Input first string");
+        String first = scanner.nextLine();
+        System.out.println("Input second string");
+        String second = scanner.nextLine();
+        System.out.println("Input third string");
+        String third = scanner.nextLine();
 
-        // Task 1
+        System.out.println(third);
+        System.out.println(second);
+        System.out.println(first);
 
-        // Task 2
+        //Task 2
+        int x = 6;
+        int y = 4;
+        int z = 7;
+        int sum = (x+y+z) / 3;
 
-        // Task 3...
+        System.out.println(sum);
+
+        //Task 3
+
+        int a = 8;
+        int b = 2;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a /b);
+      */
+        //Task 4
+        System.out.println("How are you? ");
+        String first = scanner.nextLine();
+        System.out.println("You are" + first);
 
     }
 }
