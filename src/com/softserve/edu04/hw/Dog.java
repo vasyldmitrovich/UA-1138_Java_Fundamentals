dcf866ded137ff5e84157c00e8f651f567b46a4a
Vitalii <vitalii.kravtsov@yahoo.com>
2024-02-01 17:00:44 +0200

    do hw4

---
+++ src/com/softserve/edu04/hw/Dog.java
@@ -0,0 +1,52 @@
+package com.softserve.edu04.hw;
+
+import java.util.Objects;
+
+public class Dog {
+    private String name;
+    private Breed breed;
+    private int age;
+
+    public Dog(String name, Breed breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dog dog = (Dog) o;
+        return Objects.equals(name, dog.name);
+    }
+
+
+    public enum Breed{
+        BULLDOG, BOXER, BEAGLE;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Breed getBreed() {
+        return breed;
+    }
+
+    public void setBreed(Breed breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
