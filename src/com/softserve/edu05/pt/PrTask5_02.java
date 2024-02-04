cf70439e59f8fae6ae5282a0af704fd5d72ac156
gannasamokhval <ganna.samokhval@gmail.com>
2024-02-04 09:11:19 +0200

    HW3

---
+++ src/com/softserve/edu05/pt/PrTask5_02.java
@@ -0,0 +1,27 @@
+package com.softserve.edu05.pt;
+
+import java.util.Scanner;
+
+public class PrTask5_02 {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("Enter a number: ");
+        int number = SCANNER.nextInt();
+        if (isPrime(number)) {
+            System.out.println("The number " + number + " is prime");
+        } else {
+            System.out.println("The number " + number + " is not prime");
+        }
+    }
+
+    public static boolean isPrime(int number) {
+        if (number < 0) number = -number;
+        if (number == 1) return false;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+}
