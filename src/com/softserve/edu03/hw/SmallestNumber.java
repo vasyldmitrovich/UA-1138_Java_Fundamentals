83eba99594cc9807c70ec7dbd1361da994e45bd1
GlushakOlga <oliag22@gmail.com>
2024-02-04 22:43:19 +0200

    Add Pt from edu03

---
+++ src/com/softserve/edu03/hw/SmallestNumber.java
@@ -0,0 +1,23 @@
+package com.softserve.edu03.hw;
+
+public class SmallestNumber {
+    public static void main(String[] args) {
+        int num1 = 18;
+        int num2 = 32;
+        int num3 = 54;
+
+        int smallNumber = findSmallNumber(num1, num2, num3);
+        System.out.println("The smallest number is: " + smallNumber);
+    }
+
+    public static int findSmallNumber(int num1, int num2, int num3) {
+        int smallest = num1;
+
+        if (num2 < smallest) {
+            smallest = num2;
+        }
+        if (num3 < smallest) {
+            smallest = num3;
+        }
+        return smallest;
+    }
---
+++ src/com/softserve/edu03/hw/TriangleArea.java
@@ -0,0 +1,25 @@
+package com.softserve.edu03.hw;
+
+import java.util.Scanner;
+
+public class TriangleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the base of the triangle: ");
+        double base = scanner.nextDouble();
+
+        System.out.print("Enter the height of the triangle: ");
+        double height = scanner.nextDouble();
+
+        double area = calculateTriangleArea(base, height);
+        System.out.println("The area of the triangle is: " + area);
+
+        scanner.close();
+    }
+
+    public static double calculateTriangleArea(double base, double height) {
+        return 0.5 * base * height;
+    }
+}
+
