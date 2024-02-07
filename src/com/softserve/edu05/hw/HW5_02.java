873dadbf5e8c2f8993b955b469ee3ee88944de17
gannasamokhval <ganna.samokhval@gmail.com>
2024-02-07 23:31:46 +0200

    HW5 and PT5

--- src/com/softserve/edu05/hw/HW5_02.java
+++ src/com/softserve/edu05/hw/HW5_02.java
@@ -8,17 +8,22 @@ public class HW5_02 {
 
     public static void main(String[] args) {
         boolean condition = true;
+
         do {
             System.out.println("Please, enter the first number: ");
             int num1 = SCANNER.nextInt();
+
             System.out.println("Please, enter the second number: ");
             int num2 = SCANNER.nextInt();
+
             System.out.println("The sum is " + (num1 + num2));
+
             System.out.println("Would You like to continue? (y/n)");
             String answer = SCANNER.next();
             if ("n".equals(answer)) {
                 condition = false;
             }
+
         } while (condition);
     }
 }
