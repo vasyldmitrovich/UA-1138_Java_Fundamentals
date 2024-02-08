f5df16c1b0809574e5916f29c19c8e02c676dc6d
mykytasirobaba <mykyta.sirobaba@gmail.com>
2024-02-08 13:05:11 +0200

    error correction

--- src/com/softserve/edu03/hw/person/Person.java
+++ src/com/softserve/edu03/hw/person/Person.java
@@ -64,26 +64,4 @@ public class Person {
         SCANNER.nextLine();
     }
 
-    public static void main(String[] args) {
-        Person person1 = new Person();
-        Person.input(person1);
-        person1.changeName();
-        person1.output();
-
-        Person person2 = new Person();
-        Person.input(person2);
-        person2.output();
-
-        Person person3 = new Person();
-        Person.input(person3);
-        person3.output();
-
-        Person person4 = new Person();
-        Person.input(person4);
-        person4.output();
-
-        Person person5 = new Person();
-        Person.input(person5);
-        person5.output();
-    }
 }
