d46518ff5445b4ff403c945f00726285f47f18ca
GlushakOlga <oliag22@gmail.com>
2024-02-14 23:40:12 +0200

    Add hw from edu04

---
+++ src/com/softserve/edu04/hw/ErrorsMapping.java
@@ -0,0 +1,47 @@
+package com.softserve.edu04.hw;
+
+import java.util.Scanner;
+
+public class ErrorsMapping {
+    public enum HTTPErrors {
+        Error_400("Bad Request"),
+        Error_401("Unauthorized"),
+        Error_404("Not Found"),
+        ;
+        private final String description;
+
+        HTTPErrors(String description) {
+            this.description = description;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the HTPP error number: ");
+        int errorCode = scanner.nextInt();
+
+        HTTPErrors errors = mapErrorCodeToHTTPErrors(errorCode);
+
+        if (errors != null) {
+            System.out.println("HTTP Error " + errorCode + ": " + errors.getDescription());
+        } else {
+            System.out.println("Invalid HTTP error number");
+        }
+        scanner.close();
+    }
+
+    public static HTTPErrors mapErrorCodeToHTTPErrors(int errorCode) {
+        for (HTTPErrors errors : HTTPErrors.values()) {
+            if (errors.ordinal() == errorCode - 400) {
+                return errors;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/com/softserve/edu04/hw/RangeNumbers.java
@@ -0,0 +1,22 @@
+package com.softserve.edu04.hw;
+
+import java.util.Scanner;
+
+public class RangeNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter three float numbers:");
+        float num1 = scanner.nextFloat();
+        float num2 = scanner.nextFloat();
+        float num3 = scanner.nextFloat();
+
+        boolean inRange = (num1 >= -5 && num1 <= 5) && (num2 >= -5 && num2 <= 5) && (num3 >= -5 && num3 <= 5);
+
+        if (inRange) {
+            System.out.println("Numbers are belong to the range -5. 5");
+        } else {
+            System.out.println("Not all numbers are belong to the range -5, 5");
+        }
+    }
+}
