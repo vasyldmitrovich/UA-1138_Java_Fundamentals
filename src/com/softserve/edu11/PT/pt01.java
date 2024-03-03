384e57929cbb83eb06d087b8c285dd4ae0761f20
Lucy Tatsyuk <lyuda.tatsyuk@gmail.com>
2024-03-03 15:38:55 +0200

    Add PT 11

---
+++ src/com/softserve/edu11/PT/pt01.java
@@ -0,0 +1,33 @@
+package com.softserve.edu11.PT;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class pt01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(in);
+        while (true) {
+            try {
+                System.out.println("Enter side a ");
+                int a = Integer.parseInt(scanner.nextLine());
+                System.out.println("Enter side b ");
+                int b = Integer.parseInt(scanner.nextLine());
+                int area = rectangleArea(a, b);
+                System.out.println("Area is " + area);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("This is not a number");
+            }
+        }
+
+
+    }
+
+    public static int rectangleArea(int a, int b) {
+        if (a < 0 | b < 0) {
+            throw new IllegalArgumentException("Size should be not negative");
+        }
+        return a * b;
+    }
+}
