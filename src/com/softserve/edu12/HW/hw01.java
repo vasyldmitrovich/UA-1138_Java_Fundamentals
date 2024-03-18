3711522519ae840558b212ee82477ee85077fd8a
Lucy Tatsyuk <lyuda.tatsyuk@gmail.com>
2024-03-18 16:20:33 +0200

    HW12 (01)

---
+++ src/com/softserve/edu12/HW/hw01.java
@@ -0,0 +1,33 @@
+package com.softserve.edu12.HW;
+
+import java.util.Scanner;
+
+public class hw01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter first number: ");
+            double num1 = scanner.nextDouble();
+
+            System.out.print("Enter second number: ");
+            double num2 = scanner.nextDouble();
+
+            double result = num1 / num2;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Division by zero is not allowed.");
+        }
+    }
+
+    public static double div(double num1, double num2) throws ArithmeticException {
+        if (num2 == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return num1 / num2;
+    }
+}
+
+
+
+
