af1dbfeb546ce8f855675382e8cc09da64d173dd
gannasamokhval <ganna.samokhval@gmail.com>
2024-03-22 15:04:14 +0200

    HW10

---
+++ src/com/softserve/edu10/Student.java
@@ -0,0 +1,58 @@
+package com.softserve.edu10;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class Student implements Comparable<Student> {
+
+    String name;
+    int course;
+
+    public Student(String name, int course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public static void printStudents(List<Student> students, int course) {
+        Iterator<Student> iterator = students.iterator();
+        while (iterator.hasNext()) {
+            Student student = iterator.next();
+            if (student.getCourse() == course) {
+                System.out.println(student.getName());
+            }
+        }
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        int result = getName().compareTo(o.getName());
+        if (result != 0) {
+            return result;
+        }
+        return getCourse() - o.getCourse();
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", course=" + course +
+                '}';
+    }
+}
